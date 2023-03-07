b7bc48f0322a2891c8f4560bfdb0c4a9d7be1747
Jeroen Brinkhorst <jeroenb04@live.com>
2023-03-07 14:14:45 +0100

    chore: migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_124016) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_130941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_124016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["slug"], name: "index_speakers_on_slug", unique: true
+  end
+
+  create_table "sponsors", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_sponsors_on_slug", unique: true
   end
 
 end
