775af2fe5f76563cc1c2edc113337f78e13e4b49
Jeroen B <jeroenb04@live.com>
2023-03-15 13:53:17 +0100

    feat: Content for the sponsors tab!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Add the Sponsors
+Sponsor.create(title: "Infi", body: "In early 2022, Infi opened its doors and immediately started working on really fun projects. 
+  We built a cosy office as a home for talented developers.  We are a team we are incredibly proud of: smart developers with a passion for technology, 
+  customers with world-changing ambition, and lots of fun. Only... you are still missing!
+  Want to know more? Check out our website at ", image: "sponsors/infi.svg", link: "https://infi.nl/")
+Sponsor.create(title: "Bol.com", body: "At bol.com you get the space to discover new things. 
+  Do you have a good idea or do you see something that could be improved? Get to work on it. 
+  And you certainly don’t have to do this alone. Bol.com’ers are always ready to help each other to take good plans further and to help colleagues in their development. 
+  This is how we grow together. As people, as a team and as an organisation.
+  Do you want to know more about the possibilities at bol.com? Check out ", image: "sponsors/bol.com.jpg", link: "https://careers.bol.com/en/")
+Sponsor.create(title: "Capgemini", body: "Are you ready to take on the challenge?
+  Helping our clients grow while building a more sustainable, more inclusive future is a tough ask.  
+  But when you join Capgemini, you join a thriving company and become part of a diverse global collective of free-thinkers, entrepreneurs and industry experts who are all driven to use technology to reimagine what’s possible. 
+  Are you interested? Take a look at our website: ", image: "sponsors/capgemini.svg", link: "https://www.capgemini.com/careers/")
+Sponsor.create(title: "Chipsoft", body: "Putting their shoulders to the wheel and developing the best healthcare software for healthcare. With that team spirit, Hans and Gerrit Mulder started ChipSoft in 1986. Three decades later, that no-nonsense mentality is still at the heart of the company. Even more than ever, as the Mulders now have over 900 'Chippies'.
+
+  All those hundreds of employees are given room to develop themselves through courses and training. And to develop their own ideas. Because at ChipSoft, we believe in the motto 'better well thought out than well stolen'. This freedom also applies to activities with your colleagues.
+  
+  What is typical ChipSoft? Teaching a medical specialist the intricacies of our software. But also organising a sports activity for your colleagues. Or developing a solution that optimises patient logistics in hospitals. All with one common goal: strengthening our leading position in the world of healthcare ICT. 
+  If you're interested in joining us, check out: ", image: "sponsors/chipsoft.svg", link: "https://chipsoft.nl/")
+Sponsor.create(title: "Working Talent", body: "An IT traineeship at Working Talent
+  If you know you have what it takes to become successful in the world of IT and you haven't finished learning yet. 
+  Then an IT traineeship at Working Talent is the way to go. In the IT traineeship you make the connection between your studies and the job market. 
+  From day one, you will be working with innovative techniques and tools, so you will start with confidence at one of our clients.
+  For more info: ", image: "sponsors/workingtalent.svg", link: "https://workingtalent.nl/")
+Sponsor.create(title: "YER", body: "", image: "sponsors/YER-logo.jpg", link: "https://www.yer.nl/8324175/IT.html")
