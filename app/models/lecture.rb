ea8f0a050c60638293504dbe3b5ebdb909f0e8be
Jeroen Brinkhorst <jeroenb04@live.com>
2023-04-11 11:02:38 +0200

    Initial Setup of Lectures scaffold
    - Only added index / show, because of needed.
    - Should be used with a SPA approach, or with at least a modal instead of redirecting.

---
+++ app/models/lecture.rb
@@ -0,0 +1,5 @@
+class Lecture < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  belongs_to :speaker
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lectures, only: [:index, :show], param: :slug
   devise_for :users
   resources :sponsors, only: [:index], param: :slug
   resources :speakers, only: [:index], param: :slug
