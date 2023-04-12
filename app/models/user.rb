37cc3a86fd0d763203266d102a24cb638ceeb62e
Jeroen Brinkhorst <jeroenb04@live.com>
2023-04-13 00:22:24 +0200

    und

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :lockable#, :confirmable
+         :omniauthable, :lockable, :confirmable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
