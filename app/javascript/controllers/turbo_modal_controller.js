a18fec31c219383f4865bc6534520f8f31d8f3f9
Jeroen Brinkhorst <jeroenb04@live.com>
2023-04-12 10:35:10 +0200

    feat: Setup stimulus

---
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="turbo-modal"
+export default class extends Controller {
+  connect() {
+    console.log("Turbo Modal Controller initialized");
+  }
+
+
+  hideModal() {
+    // find modal-container by id
+    this.modalTarget = document.getElementById("modal-container")
+    // remove modal-container from DOM
+    this.modalTarget.remove()
+    // remove src from turbo-frame
+    this.frameTarget = document.getElementById("modal")
+    this.frameTarget.removeAttribute("src")
+  }
+
+  // hide modal when clicking ESC
+  // action: "keyup@window->turbo-modal#closeWithKeyboard"
+  closeWithKeyboard(e) {
+    if (e.code == "Escape") {
+      this.hideModal()
+    }
+  }
+
+  // hide modal when clicking outside of modal
+  // action: "click@window->turbo-modal#closeBackground"
+  closeBackground(e) {
+    if (e && this.modalTarget.contains(e.target)) {
+      return
+    }
+    this.hideModal()
+  }
+}
